5f89c2c914bd1c7ba88adf217c2b0df835b52c29
Bilgesu Barış <bilgesubaris@gmail.com>
2025-05-07 19:29:57 +0300

    Initial Commit

---
+++ ErasmusApp/ErasmusAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErasmusAppApp.swift
+//  ErasmusApp
+//
+//  Created by Bilgesu Barış on 7.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct ErasmusAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ErasmusAppTests/ErasmusAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ErasmusAppTests.swift
+//  ErasmusAppTests
+//
+//  Created by Bilgesu Barış on 7.05.2025.
+//
+
+import Testing
+@testable import ErasmusApp
+
+struct ErasmusAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
